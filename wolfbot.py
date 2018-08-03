77deb416288c7245f14158c0e1690bce2d792d8a
Wolferz <wolferzgaming@gmail.com>
2018-08-03 03:01:38 -0400

    Added Error Handling and a Ready Check system
    It dm's me (wolferz) with error information

--- wolfbot.py
+++ wolfbot.py
@@ -6,7 +6,7 @@ import asyncio
 from time import localtime, timezone
 from discord.voice_client import VoiceClient
 
-version = '3.1'
+version = '3.2'
 
 startup_extensions = ["Music"]
 
@@ -16,21 +16,23 @@ client = Bot(bot_prefixes)
 
 dndc = '428010154743693312'  # D&D Text Channel ID
 
+
 class Main_Commands():
     def __init__(self,client):
         self.bot = client
+
 
 async def dndscheduler():
     await client.wait_until_ready()
     channel = discord.Object(id=dndc)
     while not client.is_closed:
-        hr = 18  #IN JULY (Usually 18, which equals 2PM EST (in July))
+        hr = 18  # IN JULY (Usually 18, which equals 2PM EST (in July))
         dasave = daylightSavings.daySave()
         if dasave == True:
             hr = hr
         elif dasave == False:
             hr = hr - 1
-        if localtime().tm_wday == 6 and localtime().tm_hour == hr and localtime().tm_min == 0:
+        if localtime().tm_wday == 6 and localtime().tm_hour == hr and localtime().tm_min == 0 and client.get_server() == 97414927530090496:
             await client.send_message(channel, '<@&428010612514226214>' + " HOLD ON TO YOUR BUTTS! IT'S D&D TIME" + '\n' + '<@216455910703366144>' + " Don't forget to sneak attack and use lucky ya fool!")
         await asyncio.sleep(60)  # task runs every 60 seconds
 
@@ -58,6 +60,39 @@ async def changeplay(game):
 async def cdndr(rle):
     print(rle)
     await client.say('`' + rle + '`')
+
+
+@client.command(name='rc',
+                description='A ready Check',
+                brief='Ready Check',
+                aliases=['Readycheck','readycheck'])
+async def rc(tot):
+    global ready
+    tot = int(tot)
+    if tot <= 0:
+        await client.say('You need to specify how many people we are waiting on')
+    else:
+        await client.say('Ready Check! Waiting on ' + str(tot) + 'People!' + '\n' + "Ready Up with '!r'")
+        ready = 0
+        tim = 0
+        while ready < tot:
+            if tim == (15 * 3):
+                await client.say("The ready check has timed out")
+                break
+            tim = tim + 3
+            await asyncio.sleep(3)
+        if ready >= tot:
+            await client.say('Everyone is Ready!')
+
+
+@client.command(name='r',
+                brief='Used in a Ready Check',
+                aliases=['ready'],
+                pass_context=True)
+async def r(context):
+    global ready
+    ready = ready + 1
+    await client.say(context.message.author.mention + 'has readied up!')
 
 
 @client.command(name='hello',
@@ -97,6 +132,11 @@ if __name__ == '__main__':
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
 
+@client.event
+async def on_command_error(error, ctx):
+    user = discord.utils.get(client.get_all_members(), id='257380719729311745')
+    if user is not None:
+        await client.send_message(user, 'ERROR' + '\n' + str(error))
 
 @client.event
 async def on_ready():
@@ -104,7 +144,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name='Version ' + str(version), url='about:blank', type=1))
+    await client.change_presence(game=discord.Game(name='Version ' + str(version)))
 
 client.loop.create_task(dndscheduler())
-client.run('Token')
+client.run('')
